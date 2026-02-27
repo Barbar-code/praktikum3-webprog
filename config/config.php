a8616b054aca795074c345b3ce0f83a72af55c0b
Kibar <kibarramadhan89@gmail.com>
2026-02-27 22:38:37 +0700

    buat sistem perpustakaan mini crud

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Application Configuration
-define('APP_NAME', 'LibraryX');
+define('APP_NAME', 'TeaterX');
 define('APP_VERSION', '1.0.0');
 define('BASE_URL', 'http://localhost/praktikum/');
 define('BASE_PATH', dirname(__DIR__) . '/');
@@ -9,7 +9,7 @@ define('UPLOAD_URL', BASE_URL . 'public/uploads/posters/');
 
 // Database
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'library_db');
+define('DB_NAME', 'teater_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
